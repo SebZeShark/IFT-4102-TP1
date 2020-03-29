840791056af41b10c9765e10a59cb5a1333ab47b
SolZanettol <leoguerinmorneau@gmail.com>
2020-03-29 12:35:06 -0400

    Load datasets fonctionnent

--- Code/load_datasets.py
+++ Code/load_datasets.py
@@ -12,16 +12,16 @@ def load_iris_dataset(train_ratio):
 
     Retours:
         Cette fonction doit retourner 4 matrices de type Numpy, train, train_labels, test, et test_labels
-		
+
         - train : une matrice numpy qui contient les exemples qui vont etre utilisés pour l'entrainement, chaque 
         ligne dans cette matrice représente un exemple (ou instance) d'entrainement.
-		
+
         - train_labels : contient les labels (ou les étiquettes) pour chaque exemple dans train, de telle sorte
           que : train_labels[i] est le label (ou l'etiquette) pour l'exemple train[i]
         
         - test : une matrice numpy qui contient les exemples qui vont etre utilisés pour le test, chaque 
         ligne dans cette matrice représente un exemple (ou instance) de test.
-		
+
         - test_labels : contient les labels (ou les étiquettes) pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est le label (ou l'etiquette) pour l'exemple test[i]
     """
@@ -36,9 +36,19 @@ def load_iris_dataset(train_ratio):
     
     
     # TODO : le code ici pour lire le dataset
-    
+
+    data = f.readlines()[:-2]
+    random.shuffle(data)
+    values = np.array(list(map(lambda x: list(map(lambda y: float(y), x[:15].split(','))), data)))
+    labels = np.array(list(map(lambda x: x[16:-1], data)))
+    cutoff = int(labels.size*train_ratio)
+    train = values[:cutoff]
+    train_labels = labels[:cutoff]
+    test = values[cutoff:]
+    test_labels = labels[cutoff:]
+
     # REMARQUE très importante : 
-	# remarquez bien comment les exemples sont ordonnés dans 
+    # remarquez bien comment les exemples sont ordonnés dans
     # le fichier du dataset, ils sont ordonnés par type de fleur, cela veut dire que 
     # si vous lisez les exemples dans cet ordre et que si par exemple votre ration est de 60%,
     # vous n'allez avoir aucun exemple du type Iris-virginica pour l'entrainement, pensez
@@ -48,9 +58,9 @@ def load_iris_dataset(train_ratio):
     
     # Tres important : la fonction doit retourner 4 matrices (ou vecteurs) de type Numpy. 
     return (train, train_labels, test, test_labels)
-	
-	
-	
+
+
+
 def load_congressional_dataset(train_ratio):
     """Cette fonction a pour but de lire le dataset Congressional Voting Records
 
@@ -63,13 +73,13 @@ def load_congressional_dataset(train_ratio):
         
         - train : une matrice numpy qui contient les exemples qui vont etre utilisés pour l'entrainement, chaque 
         ligne dans cette matrice représente un exemple (ou instance) d'entrainement.
-		
+
         - train_labels : contient les labels (ou les étiquettes) pour chaque exemple dans train, de telle sorte
           que : train_labels[i] est le label (ou l'etiquette) pour l'exemple train[i]
         
         - test : une matrice numpy qui contient les exemples qui vont etre utilisés pour le test, chaque 
         ligne dans cette matrice représente un exemple (ou instance) de test.
-		
+
         - test_labels : contient les labels (ou les étiquettes) pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est le label (ou l'etiquette) pour l'exemple test[i]
     """
@@ -85,13 +95,22 @@ def load_congressional_dataset(train_ratio):
     # Le fichier du dataset est dans le dossier datasets en attaché 
     f = open('datasets/house-votes-84.data', 'r')
 
-	
+
     # TODO : le code ici pour lire le dataset
-    
-	
-	# La fonction doit retourner 4 structures de données de type Numpy.
+
+    data = f.readlines()[:-1]
+    random.shuffle(data)
+    values = np.array(list(map(lambda x: x[-32:-1].split(','), data)))
+    labels = np.array(list(map(lambda x: x[0:-33], data)))
+    cutoff = int(labels.size * train_ratio)
+    train = values[:cutoff]
+    train_labels = labels[:cutoff]
+    test = values[cutoff:]
+    test_labels = labels[cutoff:]
+
+    # La fonction doit retourner 4 structures de données de type Numpy.
     return (train, train_labels, test, test_labels)
-	
+
 
 def load_monks_dataset(numero_dataset):
     """Cette fonction a pour but de lire le dataset Monks
@@ -105,9 +124,9 @@ def load_monks_dataset(numero_dataset):
 
     Args:
         numero_dataset: lequel des sous problèmes nous voulons charger (1, 2 ou 3 ?)
-		par exemple, si numero_dataset=2, vous devez lire :
-			le fichier monks-2.train contenant les exemples pour l'entrainement
-			et le fichier monks-2.test contenant les exemples pour le test
+        par exemple, si numero_dataset=2, vous devez lire :
+            le fichier monks-2.train contenant les exemples pour l'entrainement
+            et le fichier monks-2.test contenant les exemples pour le test
         les fichiers sont tous dans le dossier datasets
     Retours:
         Cette fonction doit retourner 4 matrices de type Numpy, train, train_labels, test, et test_labels
@@ -122,9 +141,19 @@ def load_monks_dataset(numero_dataset):
         - test_labels : contient les labels (ou les étiquettes) pour chaque exemple dans test, de telle sorte
           que : test_labels[i] est le label (ou l'etiquette) pour l'exemple test[i]
     """
-	
-	
-	# TODO : votre code ici, vous devez lire les fichiers .train et .test selon l'argument numero_dataset
+
+
+    # TODO : votre code ici, vous devez lire les fichiers .train et .test selon l'argument numero_dataset
+
+    f = open('datasets/monks-{}.train'.format(numero_dataset), 'r')
+    train_data = f.readlines()[:-1]
+    f = open('datasets/monks-{}.test'.format(numero_dataset), 'r')
+    test_data = f.readlines()[:-1]
+
+    train = np.array(list(map(lambda x: list(map(lambda y: int(y), x[1:14].split(' '))), train_data)))
+    train_labels = np.array(list(map(lambda x: x[15:-1], train_data)))
+    test = np.array(list(map(lambda x: list(map(lambda y: int(y), x[1:14].split(' '))), test_data)))
+    test_labels = np.array(list(map(lambda x: x[15:-1], test_data)))
 
     # La fonction doit retourner 4 matrices (ou vecteurs) de type Numpy. 
     return (train, train_labels, test, test_labels)
