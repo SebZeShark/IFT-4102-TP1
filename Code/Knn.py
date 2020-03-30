4ae6f7843e19265dc2c74651231c3b99b7781abd
SolZanettol <leoguerinmorneau@gmail.com>
2020-03-30 10:46:48 -0400

    Matrice de confusion et autres

---
+++ Code/Knn.py
@@ -0,0 +1,99 @@
+"""
+Vous allez definir une classe pour chaque algorithme que vous allez développer,
+votre classe doit contenit au moins les 3 methodes definies ici bas, 
+    * train 	: pour entrainer le modèle sur l'ensemble d'entrainement
+    * predict 	: pour prédire la classe d'un exemple donné
+    * test 		: pour tester sur l'ensemble de test
+vous pouvez rajouter d'autres méthodes qui peuvent vous etre utiles, mais moi
+je vais avoir besoin de tester les méthodes test, predict et test de votre code.
+"""
+
+import numpy as np
+from collections import Counter
+
+
+# le nom de votre classe
+# BayesNaif pour le modèle bayesien naif
+# Knn pour le modèle des k plus proches voisins
+
+class Knn:
+
+    def __init__(self, **kwargs):
+        """
+        c'est un Initializer.
+        Vous pouvez passer d'autre paramètres au besoin,
+        c'est à vous d'utiliser vos propres notations
+        """
+        self.train_list = kwargs['train']
+        self.train_labels = kwargs['train_labels']
+
+    def dist(self, e1, e2):
+        return sum(abs(x - y) for x, y in zip(e1, e2))
+
+    def predict(self, exemple, label, k=5):
+        """
+        Prédire la classe d'un exemple donné en entrée
+        exemple est de taille 1xm
+
+        si la valeur retournée est la meme que la veleur dans label
+        alors l'exemple est bien classifié, si non c'est une missclassification
+
+        """
+        distances = []
+        for i in range(self.train_labels.size):
+            distances.append((self.dist(self.train_list[i], exemple), self.train_labels[i]))
+
+        distances.sort(key=lambda x: x[0])
+        classes = map(lambda y: y[1], distances[:k])
+        class_found = Counter(classes).most_common(1)[0][0]
+        return class_found == label, class_found
+
+
+    def train_test(self, test, test_labels):
+        """
+        c'est la méthode qui va tester votre modèle sur les données de test
+        l'argument test est une matrice de type Numpy et de taille nxm, avec
+        n : le nombre d'exemple de test dans le dataset
+        m : le mobre d'attribus (le nombre de caractéristiques)
+
+        test_labels : est une matrice numpy de taille nx1
+
+        vous pouvez rajouter d'autres arguments, il suffit juste de
+        les expliquer en commentaire
+
+        Faites le test sur les données de test, et afficher :
+        - la matrice de confision (confusion matrix)
+        - l'accuracy
+        - la précision (precision)
+        - le rappel (recall)
+
+        Bien entendu ces tests doivent etre faits sur les données de test seulement
+
+        """
+        metriques = {}
+        for i in set(test_labels):
+            metriques[i] = {'TP': 0, 'TN': 0, 'FP': 0, 'FN': 0}
+
+        for i in zip(test, test_labels):
+            resultat = self.predict(i[0], i[1])
+            if resultat[0]:
+                for label, m in metriques.items():
+                    if label == i[1]:
+                        metriques[label]['TP'] += 1
+                    else:
+                        metriques[label]['TN'] += 1
+            else:
+                for label, m in metriques.items():
+                    if label == i[1]:
+                        metriques[label]['FN'] += 1
+                    elif label == resultat[1]:
+                        metriques[label]['FP'] += 1
+                    else:
+                        metriques[label]['TN'] += 1
+
+        print(metriques)
+
+
+
+    # Vous pouvez rajouter d'autres méthodes et fonctions,
+    # il suffit juste de les commenter.
